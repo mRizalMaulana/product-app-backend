d0fc0bcf2272169cb3297923d67d9fcc1acc6d41
Rizal M <mrizalmaulana000@yahoo.com>
2021-10-24 15:14:48 +0700

    setup product endpoint

---
+++ app.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+const fetch = require('node-fetch');
+const xmlParser = require('xml2json');
+require('dotenv').config();
+
+const init = async () => {
+    const server = Hapi.server({
+        port: process.env.APP_PORT,
+        host: process.env.APP_HOST
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            return fetch(`${process.env.ELEVENIA_URI}/rest/prodservices/product/listing`, {
+                headers: {
+                    'Content-Type': 'application/xml',
+                    'Accept-Charset': 'utf-8',
+                    'openapikey': process.env.ELEVENIA_API_KEY
+                }
+            })
+            .then(response => response.text())
+            .then(response => xmlParser.toJson(response))
+            .then(response => JSON.parse(response))
+            .then(products => products.Products.product)
+            .then(data => { 
+                return data.map(product => {
+                    return {
+                        productName : product.prdNm
+                    }
+                })
+             })
+            .catch(err => console.log(err));
+        }
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
